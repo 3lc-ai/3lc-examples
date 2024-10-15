6c3c15320be9e052c74ca3df4e8acd29b6276048
Gudbrand Tandberg <gudbrand@3lc.ai>
2024-10-15 15:54:53 +0200

    Improvements to add_columns_to_table

--- tools/add_columns_to_table.py
+++ tools/add_columns_to_table.py
@@ -1,5 +1,3 @@
-"""Add columns to a table in a project."""
-
 from __future__ import annotations
 
 import typing
@@ -7,14 +5,14 @@ from collections import defaultdict
 from typing import Callable
 
 import tlc
-from common import check_package_version
+from .common import check_package_version
 
 _SampleTypeStructure = typing.Union[
     tlc.SampleType, typing.Type[tlc.SampleType], list, tuple, dict, tlc.Schema, tlc.ScalarValue, Callable
 ]
 
 
-def check_columns_and_schemas(columns: dict[str, list], schemas: dict[str, _SampleTypeStructure]) -> None:
+def _check_columns_and_schemas(columns: dict[str, list], schemas: dict[str, _SampleTypeStructure]) -> None:
     assert isinstance(columns, dict), f"columns must be a dictionary, got {type(columns)}"
     assert columns, "columns must not be empty"
     first_column = next(iter(columns.values()))
@@ -29,9 +27,9 @@ def add_columns_to_table(
     output_table_name: str = "added_columns",
     description: str = "Table with added columns",
 ) -> tlc.Table:
-    """ """
+    """"""
     check_package_version("tlc", "2.9")
-    check_columns_and_schemas(columns, schemas)
+    _check_columns_and_schemas(columns, schemas)
 
     input_schemas = table.row_schema.values
     schemas.update(input_schemas)
@@ -52,23 +50,24 @@ def add_columns_to_table(
     # Copy over all rows from the input table
     for row in table.table_rows:
         for column_name, column_value in row.items():
-            data[column_name].append(column_value)
+            data[column_name].append(input_schemas[column_name].sample_from_row(column_value))
 
     # Add the new columns
     for column_name, column_values in columns.items():
         data[column_name] = column_values
 
-    assert set(len(data[column_name]) for column_name in data) == 1, "All columns must have the same length"
+    assert len({len(data[column_name]) for column_name in data}) == 1, "All columns must have the same length"
 
-    table_writer.add_batch(data)
-    new_table = table_writer.finalize()
+    with tlc.bulk_data_url_context(tlc.Url("foo"), table_writer.url):
+        table_writer.add_batch(data)
+        new_table = table_writer.finalize()
     return new_table
 
 
 if __name__ == "__main__":
     table = tlc.Table.from_dict({"col": [1, 2, 3]}, project_name="add_columns", table_name="initial", if_exists="reuse")
     columns = {"new_col": [[4], [5], [6]]}
-    schemas: dict[str, tlc.Schema] = {}
+    schemas = {}
 
     new_table = add_columns_to_table(table, columns, schemas)
     assert True
