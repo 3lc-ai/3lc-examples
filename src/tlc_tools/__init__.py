c88483d27ed6a2b3b3b5d258ed6e3cf396bc7274
Gudbrand Tandberg <gudbrand@3lc.ai>
2024-11-29 09:10:28 +0100

    Add func to __all__

--- src/tlc_tools/__init__.py
+++ src/tlc_tools/__init__.py
@@ -1,6 +1,6 @@
 """Tools for working with the 3lc package."""
 
-from .add_columns_to_table import add_columns_to_table
+from .add_columns_to_table import add_columns_to_table, add_image_metrics_to_table
 from .split import split_table
 
-__all__ = ["add_columns_to_table", "split_table"]
+__all__ = ["add_columns_to_table", "split_table", "add_image_metrics_to_table"]
